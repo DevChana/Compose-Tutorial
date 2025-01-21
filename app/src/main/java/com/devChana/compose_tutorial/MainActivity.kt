5eb744750dd4e39bc8b86787c33cc27b9b67d189
viva-sam <sam@adoc.co.kr>
2025-01-21 17:15:15 +0900

    feat: text에 padding 추가
    https://developer.android.com/codelabs/jetpack-compose-basics?authuser=1&continue=https%3A%2F%2Fdeveloper.android.com%2Fcourses%2Fpathways%2Fjetpack-compose-for-android-developers-1%3Fauthuser%3D1%23codelab-https%3A%2F%2Fdeveloper.android.com%2Fcodelabs%2Fjetpack-compose-basics&_gl=1*zmufq2*_up*MQ..*_ga*NDc3NjE5NDA5LjE3Mzc0NDY2Njc.*_ga_6HH9YJMN9M*MTczNzQ0NjY2Ni4xLjAuMTczNzQ0NjY2Ni4wLjAuODYxNDIxNTk5#3

--- app/src/main/java/com/devChana/compose_tutorial/MainActivity.kt
+++ app/src/main/java/com/devChana/compose_tutorial/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.devChana.compose_tutorial.ui.theme.ComposeTutorialTheme
 
 // Compose Basics Codelab
@@ -36,7 +37,7 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = modifier.padding(24.dp)
     )
 }
 
