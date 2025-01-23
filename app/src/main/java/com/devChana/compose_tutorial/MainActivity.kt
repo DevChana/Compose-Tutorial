e61f0f9a65d2b61668647cd4b7f5f9b7872224be
viva-sam <sam@adoc.co.kr>
2025-01-23 16:32:56 +0900

    feat: feat: 6. Creating columns and rows (2)
    row 사용해보기, Button 추가해보기, weight 적용

--- app/src/main/java/com/devChana/compose_tutorial/MainActivity.kt
+++ app/src/main/java/com/devChana/compose_tutorial/MainActivity.kt
@@ -4,17 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.devChana.compose_tutorial.ui.theme.ComposeTutorialTheme
@@ -49,16 +48,19 @@ fun MyApp(
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Surface(
         color = MaterialTheme.colorScheme.primary,
-        modifier = modifier
-            .padding(horizontal = 8.dp, vertical = 4.dp),
+        modifier = modifier.padding(horizontal = 8.dp, vertical = 4.dp),
     ) {
-        Column(
-            modifier = modifier
-                .fillMaxWidth()
-                .padding(24.dp)
-        ) {
-            Text(text = "Hello ")
-            Text(text = name)
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = Modifier.weight(1f)) {
+                Text(text = "Hello ")
+                Text(text = name)
+            }
+
+            ElevatedButton(
+                onClick = { }
+            ) {
+                Text(text = "Show more")
+            }
         }
     }
 }
