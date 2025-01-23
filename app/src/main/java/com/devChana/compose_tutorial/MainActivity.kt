f998921a2d5019d8848a39254b874efb4f715b08
viva-sam <sam@adoc.co.kr>
2025-01-23 15:54:10 +0900

    feat: 6. Creating columns and rows (1)
    column 사용해보기, modifier 조금 맛보기

--- app/src/main/java/com/devChana/compose_tutorial/MainActivity.kt
+++ app/src/main/java/com/devChana/compose_tutorial/MainActivity.kt
@@ -4,14 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.devChana.compose_tutorial.ui.theme.ComposeTutorialTheme
@@ -31,24 +34,36 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyApp(modifier: Modifier = Modifier) {
-    Surface(
-        modifier = modifier,
-        color = MaterialTheme.colorScheme.background,
-    ) {
-        Greeting("Android")
+fun MyApp(
+    modifier: Modifier = Modifier,
+    names: List<String> = listOf("World", "Compose")
+) {
+    Column(modifier = modifier.padding(4.dp)) {
+        for (name in names) {
+            Greeting(name = name)
+        }
     }
 }
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier.padding(24.dp)
-    )
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = modifier
+            .padding(horizontal = 8.dp, vertical = 4.dp),
+    ) {
+        Column(
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(24.dp)
+        ) {
+            Text(text = "Hello ")
+            Text(text = name)
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun GreetingPreview() {
     ComposeTutorialTheme {
