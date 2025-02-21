58a1ebc6634b8972aa39e1861cb8d07171760de0
viva-sam <sam@adoc.co.kr>
2025-02-21 14:41:47 +0900

    feat: 13. 더보기 텍스트 추가, 애니메이션 animateContentSize 적용

--- app/src/main/java/com/devChana/compose_tutorial/MainActivity.kt
+++ app/src/main/java/com/devChana/compose_tutorial/MainActivity.kt
@@ -35,7 +35,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.coerceAtLeast
 import androidx.compose.ui.unit.dp
 import com.devChana.compose_tutorial.ui.theme.ComposeTutorialTheme
 
@@ -86,10 +85,18 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         color = MaterialTheme.colorScheme.primary,
         modifier = modifier.padding(horizontal = 8.dp, vertical = 4.dp),
     ) {
-        Row(modifier = Modifier.padding(24.dp)) {
+        Row(
+            modifier = Modifier
+                .padding(24.dp)
+                .animateContentSize(
+                    animationSpec = spring(
+                        dampingRatio = Spring.DampingRatioMediumBouncy,
+                        stiffness = Spring.StiffnessLow,
+                    )
+                )
+        ) {
             Column(modifier = Modifier
                 .weight(1f)
-                .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello ")
                 Text(
@@ -98,12 +105,10 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
                         fontWeight = FontWeight.ExtraBold
                     ),
                 )
-            }
 
-            ElevatedButton(
-                onClick = { expanded.value = !expanded.value }
-            ) {
-                Text(text = if (expanded.value) "Show less" else "Show more")
+                if (expanded) {
+                    Text(text = "Composem ipsum")
+                }
             }
 
             Icon(
