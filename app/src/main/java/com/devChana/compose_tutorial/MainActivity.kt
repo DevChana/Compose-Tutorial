3f03d291f5bdc46afdf50a8d6377f53dd8e41aa5
viva-sam <sam@adoc.co.kr>
2025-02-21 11:10:47 +0900

    feat: 12. Styling and theming your app
    테마 컬러 추가
    
    https://developer.android.com/codelabs/jetpack-compose-basics?hl=ko&continue=https%3A%2F%2Fdeveloper.android.com%2Fcourses%2Fpathways%2Fjetpack-compose-for-android-developers-1%3Fauthuser%3D1%26hl%3Dko%23codelab-https%3A%2F%2Fdeveloper.android.com%2Fcodelabs%2Fjetpack-compose-basics&authuser=1#11

--- app/src/main/java/com/devChana/compose_tutorial/MainActivity.kt
+++ app/src/main/java/com/devChana/compose_tutorial/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.devChana.compose_tutorial
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -26,6 +27,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.coerceAtLeast
 import androidx.compose.ui.unit.dp
@@ -91,7 +93,12 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
                 .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello ")
-                Text(text = name)
+                Text(
+                    text = name,
+                    style = MaterialTheme.typography.headlineMedium.copy(
+                        fontWeight = FontWeight.ExtraBold
+                    ),
+                )
             }
 
             ElevatedButton(
@@ -131,6 +138,7 @@ fun MyAppPreview() {
     }
 }
 
+@Preview(showBackground = true, widthDp = 320, uiMode = Configuration.UI_MODE_NIGHT_YES, name = "GreetingsPreviewDark")
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 fun GreetingsPreview() {
